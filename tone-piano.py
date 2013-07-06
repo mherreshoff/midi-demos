aa9e882037f952c9e29b94ed7cc883cd14d9e1e4
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2013-07-06 15:09:36 -0700

    Added harmonics to the tone piano.

--- tone-piano.py
+++ tone-piano.py
@@ -21,7 +21,8 @@ scale_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", 
 def note_to_string(n):
   return "Octave %d note %s freq=%lf." % (int(n / 12), scale_names[n%12], note_to_frequency(n))
 
-notes = [tone.make_tone(note_to_frequency(n)) for n in range(150)]
+harmonics = [.5,.3,.2]
+notes = [tone.harmonic_tone(note_to_frequency(n), harmonics) for n in range(150)]
 def play_note(n):
   notes[n].play()
 
--- tone.py
+++ tone.py
@@ -3,12 +3,30 @@ import numpy as np
 import pygame.mixer as mixer
 import pygame.sndarray as sndarray
 
-def make_tone(freq):
+MAX_AMP = 2**15 - 1
+def as_sound(vector):
+  scaled = vector * MAX_AMP/4
+  two_channel = np.array(np.transpose(np.asmatrix([[1], [1]]) * scaled), dtype=np.int16).copy(order='C')
+  return sndarray.make_sound(array=two_channel)
+# Note: The copy(order='C') is here to prevent the error message "ValueError: ndarray is not C-contiguous"
+# As documented here: https://groups.google.com/forum/#!msg/briansupport/YmhR4rXLcBI/n-_F-nwEI6YJ
+
+def pure_tone_wave(freq):
   """Make a two channel sine wave."""
   samples_per_second, sample_size, channels = mixer.get_init()
-  assert(sample_size < 0)
-  max_amp = 2**(-sample_size - 1) - 1
   length = 1.0 # One second
-  t = np.transpose(np.asmatrix([[1], [1]]) * np.linspace(0, length, length*samples_per_second))
-  wave = np.array(np.sin(t * freq * 2 * math.pi) * max_amp/5, dtype=np.int16)
-  return sndarray.make_sound(wave)
+  t = np.linspace(0, length, length*samples_per_second)
+  return np.sin(t * freq * 2 * math.pi) 
+
+def pure_tone(freq):
+  wave = pure_tone_wave(freq)
+  return as_sound(wave)
+
+def harmonic_tone_wave(freq, harmonics):
+  waves = [h*pure_tone_wave(freq*(i+1)) for i,h in enumerate(harmonics)]
+  wave = np.sum(waves, axis=0)
+  return wave
+
+def harmonic_tone(freq, harmonics):
+  wave = harmonic_tone_wave(freq, harmonics)
+  return as_sound(wave)
