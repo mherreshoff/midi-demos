02de30cf456948ff61562ed491a9326bfd0cd3f3
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2013-07-06 17:42:56 -0700

    Got a working sound echo example in portaudio.

---
+++ portaudio/echo.cc
@@ -0,0 +1,86 @@
+/*
+ * This is a test of portaudio.  It echoes its microphone input to speaker output for ten seconds.
+ * This test is suceptible to feedback when used with regular speakers.
+ *
+ * Complile with g++ echo.cc -l portaudio -o echo
+ *
+ * Author: Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
+ */
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+
+#include "portaudio.h"
+
+#define PA_SAMPLE_TYPE      paFloat32
+const int SAMPLE_RATE = 44100;
+const int FRAMES_PER_BUFFER = 256;
+const int CHANNELS = 2;
+using namespace std;
+
+void quit (PaError err, string s) {
+  cerr << "Quit: " << s << endl;
+  cerr << "Error Message: " << Pa_GetErrorText(err) << endl;
+  Pa_Terminate();
+  exit(0);
+}
+
+static int paCallback( const void *inputBuffer, void *outputBuffer,
+                           unsigned long framesPerBuffer,
+                           const PaStreamCallbackTimeInfo* timeInfo,
+                           PaStreamCallbackFlags statusFlags,
+                           void *userData ) {
+
+  float* in = (float*) inputBuffer;
+  float* out = (float*) outputBuffer;
+  for (int i=0; i < framesPerBuffer*CHANNELS ; i++) {
+    out[i] = in[i];
+  }
+  return paContinue;
+}
+
+int main (void) {
+  int clock;
+  PaError err = Pa_Initialize();
+  if (err != paNoError) quit(err, "Couldn't initialize portaudio.");
+
+  PaStreamParameters inputParameters;
+  inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+  inputParameters.channelCount = 2;       /* stereo input */
+  inputParameters.sampleFormat = PA_SAMPLE_TYPE;
+  inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
+  inputParameters.hostApiSpecificStreamInfo = NULL;
+
+  PaStreamParameters outputParameters;
+  outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+  outputParameters.channelCount = 2;       /* stereo output */
+  outputParameters.sampleFormat = PA_SAMPLE_TYPE;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+
+  PaStream *stream;
+  err = Pa_OpenStream(
+              &stream,
+              &inputParameters,
+              &outputParameters,
+              SAMPLE_RATE,
+              FRAMES_PER_BUFFER,
+              0, /* paClipOff, */  /* we won't output out of range samples so don't bother clipping them */
+              paCallback,
+              NULL );
+  if (err != paNoError) quit(err, "Couldn't open stream.");
+
+  err = Pa_StartStream( stream );
+  if( err != paNoError ) quit(err, "Couldn't start stream.");
+
+  /* Sleep for several seconds. */
+  Pa_Sleep(10*1000);
+
+  err = Pa_StopStream( stream );
+  if( err != paNoError ) quit(err, "Couldn't stop stream.");
+  err = Pa_CloseStream( stream );
+  if( err != paNoError ) quit(err, "Couldn't close stream.");
+
+  quit(err, "Success.");
+}
